18a3f1145a59b13d301a9fd5d2401a7603d44e0d
eloketobe <sameloke@yahoo.com>
2021-08-18 10:03:38 +0100

    implement functionality for c

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,28 +20,30 @@ class Solution {
         }
     }
 
+
+
     public static int getIndexForCop(String op, ArrayList filteredOps) {
 //        if (filteredOps.indexOf(op) == 0) {
 //            System.out.println("Score list must contain at least one number");
 //            return 0;
 //        }
 
-        int index = 0;
+        int index = -1;
 
-        for (int i = 1; i<filteredOps.indexOf(op); i++){
+        for (int i = 1; i < (filteredOps.indexOf(op) + 1); i++) {
             if (isInteger((String) filteredOps.get((filteredOps.indexOf(op) - i)))) {
-                index= (filteredOps.indexOf(op) - i);
+                index = (filteredOps.indexOf(op) - i);
                 return index;
             }
         }
-        return  index;
+        return index;
     }
 
 
     public static int calPoints(String[] ops) {
-        int result = Integer.MIN_VALUE;
+        int result = 0;
 
-        String[] filteredOpsArray =  Arrays.stream(ops).filter(element -> !element.equals("")).toArray(String[]::new);
+        String[] filteredOpsArray = Arrays.stream(ops).filter(element -> !element.equals("")).toArray(String[]::new);
         ArrayList<String> filteredOpsArrayList = new ArrayList<>(Arrays.asList(filteredOpsArray));
 
         for (String op : filteredOpsArray) {
@@ -51,20 +53,26 @@ class Solution {
                 if (isBetween(Integer.parseInt(op), (-3 * 104), (3 * 104))) {
                     System.out.println("this is a number");
                 }
-            } else if (op.equalsIgnoreCase("s")) {
-                System.out.println("this is s");
             } else if (op.equalsIgnoreCase("d")) {
                 System.out.println("this is d");
             } else if (op.equalsIgnoreCase("+")) {
                 System.out.println("this is +");
             } else if (op.equalsIgnoreCase("c")) {
-
-                filteredOpsArrayList.remove(getIndexForCop(op, filteredOpsArrayList));
+                try {
+                    filteredOpsArrayList.remove(getIndexForCop(op, filteredOpsArrayList));
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                filteredOpsArrayList.remove(filteredOpsArrayList.indexOf(op));
             }
+
+
         }
 
 
         System.out.println(filteredOpsArrayList);
+
+
 
         return result;
     }
