d451f7238a620c935ebdd66ced9761e54f3f83bb
eloketobe <sameloke@yahoo.com>
2021-08-19 13:24:14 +0100

    update the result method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,11 @@ package com.company;
 import java.util.*;
 import java.lang.*;
 
-
 class Solution {
 
     public static boolean isBetween(int x, int lower, int upper) {
         return lower <= x && x <= upper;
     }
-
 
     public static boolean isInteger(String str) {
         try {
@@ -37,49 +35,34 @@ class Solution {
                         break;
                     }
                 }
-
                 break;
             }
         }
 
         System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is " + (firstNumber + secondNumber));
         return firstNumber + secondNumber;
-
     }
-
-
-
 
     public static int doubleLastInteger(String op, ArrayList filteredOps) {
         int lastNumber = 0;
-
-
         for (int i = 1; i < (filteredOps.indexOf(op) + 1); i++) {
             if (isInteger((String) filteredOps.get((filteredOps.indexOf(op) - i)))) {
 
                 lastNumber = Integer.parseInt((String) filteredOps.get((filteredOps.indexOf(op) - i)));
                 System.out.println("First number is " + lastNumber);
-
-
                 break;
             }
         }
-
-        System.out.println("Double of " + lastNumber +   " is " + (lastNumber *2));
-        return lastNumber*2 ;
-
+        System.out.println("Double of " + lastNumber + " is " + (lastNumber * 2));
+        return lastNumber * 2;
     }
-
-
 
     public static int getIndexForCop(String op, ArrayList filteredOps) {
 //        if (filteredOps.indexOf(op) == 0) {
 //            System.out.println("Score list must contain at least one number");
 //            return 0;
 //        }
-
         int index = -1;
-
         for (int i = 1; i < (filteredOps.indexOf(op) + 1); i++) {
             if (isInteger((String) filteredOps.get((filteredOps.indexOf(op) - i)))) {
                 index = (filteredOps.indexOf(op) - i);
@@ -90,45 +73,22 @@ class Solution {
     }
 
 
-    public static ArrayList<Integer> getIntegerArray(ArrayList<String> stringArray) {
-        ArrayList<Integer> integerList = new ArrayList<Integer>();
-        for(String stringValue : stringArray) {
-
-                integerList.add(Integer.parseInt(stringValue));
-
-        }
-        return integerList;
-    }
-
 
     public static int calPoints(String[] ops) {
-        int result =0;
-
+        int result;
         String[] filteredOpsArray = Arrays.stream(ops).filter(element -> !element.equals("")).toArray(String[]::new);
         ArrayList<String> filteredOpsArrayList = new ArrayList<>(Arrays.asList(filteredOpsArray));
-
         for (String op : filteredOpsArray) {
-
             if (isInteger(op)) {
-
                 if (isBetween(Integer.parseInt(op), (-3 * 104), (3 * 104))) {
                     System.out.println("this is a number");
                 }
             } else if (op.equalsIgnoreCase("d")) {
                 System.out.println("this is d");
-
                 filteredOpsArrayList.set(filteredOpsArrayList.indexOf(op), String.valueOf(doubleLastInteger(op, filteredOpsArrayList)));
-
-
-
-
             } else if (op.equalsIgnoreCase("+")) {
-
                 System.out.println("this is +");
-
                 filteredOpsArrayList.set(filteredOpsArrayList.indexOf(op), String.valueOf(getSumOfLastTwoIntegers(op, filteredOpsArrayList)));
-
-
             } else if (op.equalsIgnoreCase("c")) {
                 try {
                     filteredOpsArrayList.remove(getIndexForCop(op, filteredOpsArrayList));
@@ -139,20 +99,11 @@ class Solution {
             } else {
                 System.out.println("Invalid entry");
                 filteredOpsArrayList.remove(op);
-
             }
-
         }
         System.out.println(filteredOpsArrayList);
 
-
-
-
-
-
-        for(int op : getIntegerArray(filteredOpsArrayList)) {
-            result += op;
-        }
+        result = filteredOpsArrayList.stream().mapToInt(x -> Integer.parseInt(x)).sum();
 
 
         return result;
@@ -160,7 +111,6 @@ class Solution {
 }
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] ops = sc.nextLine().split(" ");
